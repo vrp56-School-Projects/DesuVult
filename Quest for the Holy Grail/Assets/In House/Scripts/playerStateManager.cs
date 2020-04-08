576ac28aec3e02b8bc094e251adb007fb5243fa0
= <zacharylott94@gmail.com>
2020-04-08 18:26:27 -0500

    Added scripts for stats

---
+++ Quest for the Holy Grail/Assets/In House/Scripts/playerStateManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerStateManager : MonoBehaviour
+{
+
+    public CharacterController controller;
+    public Stat health;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (health.value == 0) {controller.enabled = false;}
+    }
+}
---
+++ Quest for the Holy Grail/Assets/In House/Scripts/stats/Stat.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stat : MonoBehaviour
+{
+    public int value = 100;
+    readonly int[] limits = {0,100};
+
+    public bool limitRange = true;
+
+    public void Damage(int amount) {
+        subtract(amount);
+        clamp();
+    }
+
+    public void subtract(int amount) {
+        value -= amount;
+        clamp();
+    }
+
+    public void add(int amount) {
+        value += amount;
+        clamp();
+    }
+
+    void clamp() {
+        if (limitRange) {
+            value = Mathf.Clamp(value, limits[0], limits[1]);
+        }
+    }
+}
