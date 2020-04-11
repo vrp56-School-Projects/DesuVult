a673d0fa90914b4344bae1f178aeea469730dad2
vrp56 <38020844+vrp56@users.noreply.github.com>
2020-04-11 15:39:49 -0500

    Functional Samurai AI
    The Samurai will start to head toward the player if they are within aggro range and will attack at a rate of 1.5 sec if within an attack range. If the player moves out of aggro range of a samurai, that samurai will no longer reserve the attack slot they had and will stop moving toward the player

---
+++ Quest for the Holy Grail/Assets/In House/Scripts/NinjaAttackSlotManager.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NinjaAttackSlotManager : MonoBehaviour
+{
+    private List<GameObject> _slots;
+    [SerializeField]
+    public int count = 4;
+    [SerializeField]
+    public float distance = 6f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _slots = new List<GameObject>();
+        for (int i = 0; i < count; ++i)
+        {
+            _slots.Add(null);
+        }
+    }
+
+    public Vector3 GetSlotPosition(int index)
+    {
+        float degreesPerIndex = -230f / count;
+        Vector3 pos = transform.position;
+        Vector3 offset = new Vector3(0f, 0f, distance);
+        return pos + (Quaternion.Euler(new Vector3(0f, (degreesPerIndex * index), 0f)) * offset);
+
+    }
+
+    public int Reserve(GameObject attacker)
+    {
+        Vector3 bestPosition = transform.position;
+        Vector3 offset = (attacker.transform.position - bestPosition).normalized * distance;
+        bestPosition += offset;
+        int bestSlot = -1;
+        float bestDist = -99999f;
+        for (int index = 1; index < _slots.Count-1; ++index)
+        {
+            if (_slots[index] != null)
+                continue;
+            var dist = (GetSlotPosition(index) - bestPosition).sqrMagnitude;
+            if (dist > bestDist)
+            {
+                bestSlot = index;
+                bestDist = dist;
+            }
+        }
+        if (bestSlot != -1)
+            _slots[bestSlot] = attacker;
+        return bestSlot;
+    }
+
+    public void Release(int slot)
+    {
+        _slots[slot] = null;
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        for (int index = 0; index < count; ++index)
+        {
+            if (_slots == null || _slots.Count <= index || _slots[index] == null)
+                Gizmos.color = Color.green;
+            else
+                Gizmos.color = Color.red;
+            Gizmos.DrawWireSphere(GetSlotPosition(index), 0.5f);
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Quest for the Holy Grail/Assets/In House/Scripts/SamuraiController.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class SamuraiController : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _target;
+    [SerializeField]
+    private float _attackRate = 1.5f;
+    
+    private Transform _player;
+    private NavMeshAgent _navMeshAgent;
+    private SamuraiAttackSlotManager _slotManager;
+    private int _slot = -1;
+    private float _pathtime = 0f;
+    private float _attackTime = 0f;
+    private float _attackRange = 15.0f;
+    private float _attackDistance = 0f;
+    private float _distance;
+
+   
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Set variables for the AI to use
+        _player = GameObject.FindGameObjectWithTag("Player").transform;
+        _navMeshAgent = GetComponent<NavMeshAgent>();
+        _slotManager = _player.GetComponentInParent<SamuraiAttackSlotManager>();
+        _attackDistance = _slotManager.distance + 0.5f;
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Determine if player is within aggro range
+        _distance = Vector3.Distance(transform.position, _player.position);
+
+        _attackTime += Time.deltaTime;
+        _pathtime += Time.deltaTime;
+        if (_pathtime > 0.5f)
+        {
+            _pathtime = 0f;
+            // Only move attempt to reserve slot if withing aggro range
+            if (_distance <= _attackRange)
+            {
+                
+                if (_slotManager != null)
+                {
+                    if (_slot == -1)
+                    {
+                        _slot = _slotManager.Reserve(gameObject);
+                    }
+                    if (_slot == -1)
+                        return;
+                    // Set the destination for the AI (player location)
+                    if (_navMeshAgent == null)
+                    {
+                        return;
+                    }
+                    _navMeshAgent.SetDestination(_slotManager.GetSlotPosition(_slot));
+                }
+            }
+            else if((_distance > _attackRange) && (_slot != -1))
+            {
+                _slotManager.Release(_slot);
+                _slot = -1;
+                // Reset the destination for the AI (Not following)
+                if (_navMeshAgent == null)
+                {
+                    return;
+                }
+                _navMeshAgent.ResetPath();
+            }
+        }
+
+        // Attack the Player if in attack slot
+        if (_navMeshAgent == null)
+        {
+            return;
+        }
+        else if (_distance <= _attackDistance && _attackTime > _attackRate)
+        {
+            _attackTime = 0f;
+            Debug.Log(gameObject.name + " Attacked player");
+        }
+
+        if (_player.GetComponentInParent<playerMovement>().isGrounded)
+        {
+            transform.LookAt(_target.transform);
+        }
+        
+    }
+}
