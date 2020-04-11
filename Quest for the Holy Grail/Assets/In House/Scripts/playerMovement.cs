39c912172788eb196138143b10f8527838e865cc
= <zacharylott94@gmail.com>
2020-04-10 20:24:45 -0500

    Fixed player getting stuck jumping when against wall Fixed near culling on camera Player now bumps head on ceilings and loses upward velocity

--- Quest for the Holy Grail/Assets/In House/Scripts/playerMovement.cs
+++ Quest for the Holy Grail/Assets/In House/Scripts/playerMovement.cs
@@ -11,15 +11,11 @@ public class playerMovement : MonoBehaviour
     public float jumpHeight = 2f;
 
     public bool isGrounded;
+    private float tempSlopeLimit;
+    private float tempStepOffset;
 
-    // Update is called once per frame
     void Update()
     {
-        isGrounded = (controller.collisionFlags == CollisionFlags.Below);
-        if(isGrounded && velocity.y < 0)
-        {
-          velocity.y = -2f;
-        }
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
@@ -29,11 +25,38 @@ public class playerMovement : MonoBehaviour
 
         if (Input.GetButtonDown("Jump") && isGrounded)
         {
+
+            //this block helps prevent the player from getting stuck on walls
+            tempSlopeLimit = controller.slopeLimit;
+            controller.slopeLimit = 90f;
+            tempStepOffset = controller.stepOffset;
+            controller.stepOffset = .1f;
+
+
             velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
         }
 
-        velocity.y += gravity * Time.deltaTime;
 
+        velocity.y += gravity * Time.deltaTime;
         controller.Move(velocity * Time.deltaTime);
+
+
+        isGrounded = ((controller.collisionFlags & CollisionFlags.Below) != 0);
+
+        //If you land on the ground, don't accumulate negative velocity
+        //OR
+        //If you bump your head, lose upward velocity
+        if((isGrounded && velocity.y < 0)||((controller.collisionFlags & CollisionFlags.Above)!= 0))
+        {
+
+            //reset our parameters
+            controller.slopeLimit = tempSlopeLimit;
+            controller.stepOffset = tempStepOffset;
+
+
+            velocity.y = 0f;
+        }
+
     }
+
 }
