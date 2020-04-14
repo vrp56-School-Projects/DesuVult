018e7b50dec0d408d410ef57dfce6a49243ab2cc
vrp56 <38020844+vrp56@users.noreply.github.com>
2020-04-14 18:58:06 -0500

    tried to refactor samurai controller. Failed attempt exists in comments for revisiting

--- Quest for the Holy Grail/Assets/In House/Scripts/SamuraiController.cs
+++ Quest for the Holy Grail/Assets/In House/Scripts/SamuraiController.cs
@@ -27,8 +27,8 @@ public class SamuraiController : MonoBehaviour
     private enum _samuraiState
     {
         IDLE,
-        ATTACKSLOT,
-        WAITSLOT
+        ATTACK_SLOT,
+        WAIT_SLOT
     }
 
     // track the current state of the samurai
@@ -43,11 +43,136 @@ public class SamuraiController : MonoBehaviour
         _playerHealthScript = _player.GetComponentInChildren<Health>();
         _navMeshAgent = GetComponent<NavMeshAgent>();
         _slotManager = _player.GetComponentInParent<SamuraiAttackSlotManager>();
-        _anim = this.GetComponent<Animator>();
+        _anim = this.GetComponentInParent<Animator>();
         _attackDistance = _slotManager.distance + 0.5f;
     }
 
+    // if the samurai is idle i need to check if the player is within aggro range
+    // then i need to check for an open attack slot and reserve it if possible
+    // if no open attack slot, fill a wait slot 
+    // attack when in range if in attack slot at specified rate (NEED TO FIND ATTACK ANIMS)
+    // should also do coroutines for timing 
 
+    //private void IdleState()
+    //{
+    //    if (_distance <= _attackRange)
+    //    {
+    //        // try to fill attack slot 
+    //        // if attack slot open, set destination and set state to attackSlot
+    //        // if attack slot not open, set state to waitSlot and go set destintaion to a wait slot
+
+    //        FillAttackSlot();
+    //        // if (_slot == -1) FillWaitSlot();
+    //    }
+    //}
+
+    //private void FillAttackSlot()
+    //{
+    //    if (_slotManager != null)
+    //    {
+    //        if (_slot == -1)
+    //        {
+    //            _slot = _slotManager.Reserve(gameObject);
+    //        }
+    //        if (_slot == -1)
+    //        {
+    //            _currentState = _samuraiState.WAIT_SLOT;
+    //            return;
+    //        }
+
+    //        // Set the destination for the AI (player location)
+    //        if (_navMeshAgent == null)
+    //        {
+    //            return;
+    //        }
+    //        _navMeshAgent.SetDestination(_slotManager.GetSlotPosition(_slot));
+    //        _currentState = _samuraiState.ATTACK_SLOT;
+    //    }
+    //}
+
+    //private void AttackState()
+    //{
+    //    // check if out of aggro range and release slot if so otherwise
+    //    // run to player if not at slot yet
+    //    // if at slot, attack at specified rate
+
+    //    if (_distance > _attackRange)
+    //    {
+    //        _slotManager.Release(_slot);
+    //        _slot = -1;
+    //        // Reset the destination for the AI (Not following)
+    //        if (_navMeshAgent == null)
+    //        {
+    //            return;
+    //        }
+    //        _navMeshAgent.ResetPath();
+    //        _currentState = _samuraiState.IDLE;
+    //    }
+    //    // Set the destination for the AI (player location)
+    //    if (_navMeshAgent == null)
+    //    {
+    //        return;
+    //    }
+    //    // run toward the player
+    //    if (_navMeshAgent.remainingDistance > 0f)
+    //    {
+    //        _navMeshAgent.SetDestination(_slotManager.GetSlotPosition(_slot));
+    //        _anim.SetBool("Run", true);
+    //    }
+    //    else _anim.SetBool("Run", false);
+
+    //    // attack the player
+    //    if (_distance <= _attackDistance && _attackTime > _attackRate)
+    //    {
+    //        _attackTime = 0f;
+    //        _playerHealthScript.damage(5f);
+    //        // Debug.Log(gameObject.name + " Attacked player");
+    //    }
+    //}
+
+    //private void FillWaitSlot()
+    //{
+    //    // for now go into idle state
+    //    _currentState = _samuraiState.IDLE;
+    //}
+
+    //private void WaitState()
+    //{
+    //    // go into idle state for now. Shouldn't even get here but redundancy is good
+    //    _currentState = _samuraiState.IDLE;
+
+    //    // continue checking if an attack slot opens up and try to fill when open
+    //}
+
+    //private void Update()
+    //{
+    //    Debug.Log(gameObject.name + " Current State: " + _currentState);
+
+    //    // Determine distance between player and samurai
+    //    _distance = Vector3.Distance(transform.position, _player.position);
+
+    //    // update attack time
+    //    _attackTime += Time.deltaTime;
+
+    //    // look at the player
+    //    if (_player.GetComponentInParent<playerMovement>().isGrounded)
+    //    {
+    //        transform.LookAt(_target.transform);
+    //    }
+
+    //    switch (_currentState)
+    //    {
+    //        case _samuraiState.IDLE:
+    //            IdleState();
+    //            break;
+    //        case _samuraiState.ATTACK_SLOT:
+    //            AttackState();
+    //            break;
+    //        case _samuraiState.WAIT_SLOT:
+    //            WaitState();
+    //            break;
+    //    }
+    //}
 
     // Update is called once per frame
     void Update()
@@ -63,7 +188,7 @@ public class SamuraiController : MonoBehaviour
             // Only move attempt to reserve slot if withing aggro range
             if (_distance <= _attackRange)
             {
-                
+
                 if (_slotManager != null)
                 {
                     if (_slot == -1)
@@ -80,7 +205,7 @@ public class SamuraiController : MonoBehaviour
                     _navMeshAgent.SetDestination(_slotManager.GetSlotPosition(_slot));
                 }
             }
-            else if((_distance > _attackRange) && (_slot != -1))
+            else if ((_distance > _attackRange) && (_slot != -1))
             {
                 _slotManager.Release(_slot);
                 _slot = -1;
@@ -115,7 +240,5 @@ public class SamuraiController : MonoBehaviour
             _anim.SetBool("Run", true);
         }
         else _anim.SetBool("Run", false);
-        
-
     }
 }
