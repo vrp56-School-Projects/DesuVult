e8115f92e2ddf6c6ea2f25d18158f663d89ac2ad
Warren <warren411@gmail.com>
2020-04-24 21:36:49 -0500

    pause menu & main menu update

--- Quest for the Holy Grail/Assets/In House/Scripts/UI Controllers/HoverSound.cs
+++ Quest for the Holy Grail/Assets/In House/Scripts/UI Controllers/HoverSound.cs
@@ -1,12 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.EventSystems;
 
 public class HoverSound : MonoBehaviour, IPointerEnterHandler
 {
     public void OnPointerEnter(PointerEventData eventData)
     {
-        GetComponent<AudioSource>().Play();
+        if(GetComponentInParent<CanvasGroup>().alpha == 1)
+        {
+            GetComponent<AudioSource>().Play();
+        }
     }
 }
--- Quest for the Holy Grail/Assets/PausePage.cs
+++ Quest for the Holy Grail/Assets/PausePage.cs
@@ -7,16 +7,18 @@ public class PausePage : OptionsPage
 {
     public Text ActiveQuestsText, CompletedQuestsText;
 
-    void Awake()
+    void OnEnable()
     {
         // ActiveQuestsText.text = Get the active quests
         // CompletedQuestsText.text = Get the number of completed quests
+        Time.timeScale = 0;
         
+        Initialize();
     }
 
     public void Unpause()
     {
-        // unpause game
+        Time.timeScale = 1;
         gameObject.SetActive(false);
     }
 }
