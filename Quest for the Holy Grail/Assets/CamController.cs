b7f8bf60d54188a54eb33f1a32f839f0d98ede62
Warren <warren411@gmail.com>
2020-04-25 21:26:32 -0500

    CamController

---
+++ Quest for the Holy Grail/Assets/CamController.cs
@@ -0,0 +1,136 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamController : MonoBehaviour
+{
+
+    [System.Serializable]
+    public class action
+    {
+        public float[] startLoc = new float[3];
+        public float[] endLoc = new float[3];
+        public float movDelay, movDuration;
+
+        public float[] startRot = new float[3];
+        public float[] endRot = new float[3];
+        public float rotDelay, rotDuration;
+        
+    }
+
+    public action[] actions;
+
+    public int[] ints;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Transform(actions));
+        StartCoroutine(Rotate(actions));
+    }
+
+    // transform and repeat control
+    IEnumerator Transform(action[] a)
+    {
+        // initialize
+        float newX = a[0].startLoc[0];
+        float newY = a[0].startLoc[1];
+        float newZ = a[0].startLoc[2];
+
+        transform.position = new Vector3(newX, newY, newZ);
+
+        // delay
+        yield return new WaitForSeconds(a[0].movDelay);
+
+        // do action
+        float start = Time.time;
+
+        while(true)
+        {
+            float percent = (Time.time - start)/a[0].movDuration;
+
+            // if 100%, end transform
+            if(percent >= 1)
+            {
+                newX = a[0].endLoc[0];
+                newY = a[0].endLoc[1];
+                newZ = a[0].endLoc[2];
+
+                transform.position = new Vector3(newX, newY, newZ);
+
+                break;
+            }
+
+            // else, move
+            else
+            {
+                newX = Mathf.Lerp(a[0].startLoc[0], a[0].endLoc[0], percent);
+                newY = Mathf.Lerp(a[0].startLoc[1], a[0].endLoc[1], percent);
+                newZ = Mathf.Lerp(a[0].startLoc[2], a[0].endLoc[2], percent);
+
+                transform.position = new Vector3(newX, newY, newZ);
+            }
+            yield return new WaitForEndOfFrame();
+        }
+
+        // wait for rotation to finish
+        yield return new WaitForSeconds((a[0].rotDelay + a[0].rotDuration) - (a[0].movDelay + a[0].movDuration));
+
+        // remove action
+        var a_list = new List<action>(a);
+        a_list.RemoveAt(0);
+
+        // execute again if list isn't empty
+        if(a.Length-1 > 0)
+        {
+            StartCoroutine(Transform(a_list.ToArray()));
+            StartCoroutine(Rotate(a_list.ToArray()));
+        }
+    }
+
+    // rotation control
+    IEnumerator Rotate(action[] a)
+    {
+        // initialize
+        float newX = a[0].startRot[0];
+        float newY = a[0].startRot[1];
+        float newZ = a[0].startRot[2];
+
+        transform.rotation = Quaternion.Euler(newX, newY, newZ);
+
+        // delay
+        yield return new WaitForSeconds(a[0].rotDelay);
+
+        // do action
+        float start = Time.time;
+
+        while(true)
+        {
+            float percent = (Time.time - start)/a[0].rotDuration;
+
+            // if 100%, end transform
+            if(percent >= 1)
+            {
+                newX = a[0].endRot[0];
+                newY = a[0].endRot[1];
+                newZ = a[0].endRot[2];
+
+                transform.rotation = Quaternion.Euler(newX, newY, newZ);
+
+                break;
+            }
+
+            // else, move
+            else
+            {
+                newX = Mathf.Lerp(a[0].startRot[0], a[0].endRot[0], percent);
+                newY = Mathf.Lerp(a[0].startRot[1], a[0].endRot[1], percent);
+                newZ = Mathf.Lerp(a[0].startRot[2], a[0].endRot[2], percent);
+
+                transform.rotation = Quaternion.Euler(newX, newY, newZ);
+            }
+            yield return new WaitForEndOfFrame();
+        }
+    }
+
+}
