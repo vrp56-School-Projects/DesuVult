b67c910b93dbb5e8dfb425d56812ae0b132ef78c
SheltonEllis <60905093+SheltonEllis@users.noreply.github.com>
2020-04-26 18:07:29 -0500

    Changes

---
+++ Quest for the Holy Grail/Assets/In House/Scripts/EnemyDV.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyDV : MonoBehaviour
+{
+    public bool inWindZone = false;
+    public GameObject windZone;
+    public GameObject[] enemy;
+    Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void FixedUpdate()
+    {
+        if (inWindZone)
+        {
+            rb.AddForce(transform.forward * windZone.GetComponent<WindArea>().strength);
+        }
+    }
+
+    private void OnTriggerEnter(Collider coll)
+    {
+        if(coll.gameObject.tag == "windArea")
+        {
+            windZone = coll.gameObject;
+            inWindZone = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider coll)
+    {
+        if(coll.gameObject.tag == "windArea")
+        {
+            inWindZone = false;
+        }
+    }
+
+}
