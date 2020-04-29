002010684e79346bf26ba08a9031eb39f7e6daee
Warren <warren411@gmail.com>
2020-04-29 02:30:07 -0500

    you lose prefab

---
+++ Quest for the Holy Grail/Assets/YouLoseManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class YouLoseManager : MonoBehaviour
+{
+
+    public void GiveUp()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void TryAgain()
+    {
+        SceneManager.LoadScene("IntroCutscene");
+    }
+}
