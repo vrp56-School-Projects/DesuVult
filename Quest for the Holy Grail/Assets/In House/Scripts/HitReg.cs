b826478686fa3b52ba7215b32996772e57672c75
= <zacharylott94@gmail.com>
2020-04-29 14:24:31 -0500

    Projectiles They can fly straight They can deal damage They can collide with the environment and go away

---
+++ Quest for the Holy Grail/Assets/In House/Scripts/HitReg.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitReg : MonoBehaviour, IOnHit
+{
+    private Health health;
+    void Start()
+    {
+        health = gameObject.GetComponent<Health>();
+    }
+
+    public void OnHit(float damage){
+        health.damage(damage);
+    }
+}
