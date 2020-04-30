03f0254586c48e03a70e3e8bc4441c24c3fe0daa
Viraj Patel <vrp56@msstate.edu>
2020-04-29 20:06:36 -0500

    Added damage stat script

---
+++ Quest for the Holy Grail/Assets/In House/Scripts/stats/DamageStatOnHit.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageStatOnHit : MonoBehaviour, IOnHit
+{
+    [SerializeField] private Stat stat;
+
+    public void OnHit(float damage)
+    {
+        stat.damage(damage);
+    }
+}
