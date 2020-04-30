3b267a283d92b7e788a6e46758a1b4847f0c7c4b
Warren <warren411@gmail.com>
2020-04-29 21:41:04 -0500

    enemy health bar

---
+++ Quest for the Holy Grail/Assets/EnemyHealthBar.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnemyHealthBar : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(GetComponent<Slider>().value == 0)
+        {
+            GetComponent<CanvasGroup>().alpha = 0;
+        }
+        else
+        {
+            GetComponent<CanvasGroup>().alpha = 1;
+        }
+    }
+}
