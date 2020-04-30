4825d60b3178056085437646967050f84dae8965
Warren <warren411@gmail.com>
2020-04-29 21:03:26 -0500

    notification panel

---
+++ Quest for the Holy Grail/Assets/NotificationPanel.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NotificationPanel : MonoBehaviour
+{
+
+    [SerializeField] private Text notifcationText;
+
+    // Start is called before the first frame update
+    void OnEnable()
+    {
+        StartCoroutine(ShowNotification());
+    }
+
+    public void SetNotification(string notification)
+    {
+        notifcationText.text = notification;
+        
+    }
+
+    IEnumerator ShowNotification()
+    {
+        GetComponent<FadeObject>().StartFade(0);
+        yield return new WaitForSeconds(1.5f);
+
+        yield return new WaitForSeconds(5);
+        GetComponent<FadeObject>().StartFade(1);
+    }
+}
